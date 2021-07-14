67dee65ec2a9b2c14b2bb21847bbb37b7853a67d
CallMeAtomfried <63309028+CallMeAtomfried@users.noreply.github.com>
2021-07-14 21:46:22 +0200

    Add files via upload

--- util/game.js
+++ util/game.js
@@ -28,16 +28,20 @@ module.exports = class Game {
 	
 	nextMove(message) {
 		if(this.currentTurn<10) {
+			// Cleaning up input
 			var input = message.content.replace(/ /g,"");
 			
+			// Validating input
 			var newIn = []
 			var validInput = false;
 			if(input.length == 8) {
 				newIn = [input[0]+input[1],input[2]+input[3],input[4]+input[5],input[6]+input[7]]
+				// pls disregard, checking pairs of chars bc thats what the circles are
 				validInput = this.mastermindEmotes.includes(input[0]+input[1]) && this.mastermindEmotes.includes(input[2]+input[3]) && this.mastermindEmotes.includes(input[4]+input[5]) && this.mastermindEmotes.includes(input[6]+input[7])
 			}
 			var out = ""
 			if(validInput) {
+				// for every circle in input
 				for(var x in newIn) {
 					if(this.masterMindSeq[x] == newIn[x]) {
 						out += this.checkmarks[1];
@@ -47,13 +51,14 @@ module.exports = class Game {
 						}
 					} else if(this.masterMindSeq[x] != newIn[x] && this.masterMindSeq.includes(newIn[x])) {
 						out += this.checkmarks[0];
-						this.score += (10-this.currentTurn)*5;
+						// this.score += (10-this.currentTurn)*5;
 					} else {
+						this.score -= (5*this.currentTurn);
 						out += this.checkmarks[2];
 					}
 				}
 				this.currentTurn++;
-				message.channel.send(`Turn ${this.currentTurn}: ${out}`);
+				message.channel.send(`Turn ${this.currentTurn}: ${out}, score: ${this.score}`);
 				if(out == "✅✅✅✅") {
 					message.reply("You're winner! Score: " + this.score);
 					this.currentTurn = 11;

3611c18918e4f74500492415c078a5525c5d4045
callmedarkfried <87673630+callmedarkfried@users.noreply.github.com>
2021-08-09 17:51:26 +0200

    bugfix
    Restarting the bot didnt kill the original processes causing two instances of the bot to run

---
+++ wd_new.js
@@ -0,0 +1,256 @@
+const child = require("child_process");
+
+//Heartbeat tracking
+setInterval(function(){tick()}, 1000);
+
+function callback(err) {
+	if (err) throw err;
+}
+
+var markov;
+var admin;
+var user;
+var game;
+var main;
+start();
+
+function tick() {
+	if (markov.check) markov.heartbeat++;
+	if (main.check) main.heartbeat++;
+	if (user.check) user.heartbeat++;
+	if (admin.check) admin.heartbeat++;
+	if (game.check) game.heartbeat++;
+
+	if (markov.heartbeat > 60) {
+		console.log("Markov Timeout");
+		shutdownProcess(markov)
+		markov = startProcess("./bot_markov.js")
+	}
+	if (main.heartbeat > 30) {
+		console.log("Main Timeout");
+		shutdownProcess(main)
+		main = startProcess("./main.js")
+	}
+	if (user.heartbeat > 30) {
+		console.log("User Timeout");
+		shutdownProcess(user)
+		user = startProcess("./user.js")
+	}
+	if (markov.heartbeat > 20) {
+		console.log("Admin Timeout");
+		shutdownProcess(admin)
+		admin = startProcess("./admin.js")
+	}
+	if (game.heartbeat > 60) {
+		console.log("Game Timeout");
+		shutdownProcess(game)
+		game = startProcess("./game.js")
+	}
+	
+}
+
+function shutdownProcess(proc) {
+	proc.process.send({action: "command", command: "shutdown"})
+}
+
+function startProcess(procName) {
+	return {process: child.fork(procName), invoked: false, heartbeat: 0, check: false};
+}
+
+function restart(message) {
+	// message: {command: "restart"}
+		shutdownProcess(markov)
+		shutdownProcess(main)
+		shutdownProcess(user)
+		shutdownProcess(game)
+		shutdownProcess(admin)
+		start();
+}
+
+function start() {
+	markov = startProcess("./bot_markov.js")
+	main = startProcess("./main.js")
+	user = startProcess("./user.js")
+	admin = startProcess("./admin.js")
+	game = startProcess("./game.js")
+
+	// ERROR HANDLING
+	markov.process.on('error', function(err) {
+		markov = processerror(markov);
+	});
+
+	admin.process.on('error', function(err) {
+		admin = processerror(admin);
+	});
+
+	user.process.on('error', function(err) {
+		user = processerror(user);
+	});
+	
+	main.process.on('error', function(err) {
+		main = processerror(main);
+	});
+	
+	game.process.on('error', function(err) {
+		game = processerror(game);
+	});
+
+
+	// MESSAGE  HANDLING
+	markov.process.on('message', function (message) {
+		switch (message.action) {
+			case "command":
+				sendMessageToTarget(message);
+				console.log("game:".padEnd(10), message);
+				break;
+			case "heartbeat":
+				markov.check = true;
+				markov.heartbeat = 0;
+				break;
+			case "return":
+				console.log("markov:".padEnd(10), message);
+		}
+	});
+
+	admin.process.on('message', function (message) {
+		switch (message.action) {
+			case "command":
+				sendMessageToTarget(message);
+				console.log("admin:".padEnd(10), message);
+				break;
+			case "heartbeat":
+				admin.check = true;
+				admin.heartbeat = 0;
+				break;
+			case "return":
+				console.log("admin:".padEnd(10), message);
+		}
+	});
+
+	game.process.on('message', function (message) {
+		switch (message.action) {
+			case "command":
+				sendMessageToTarget(message);
+				console.log("game:".padEnd(10), message);
+				break;
+			case "heartbeat":
+				game.check = true;
+				game.heartbeat = 0;
+				break;
+			case "return":
+				console.log("game:".padEnd(10), message);
+		}
+		
+	});
+
+	main.process.on('message', function (message) {
+		switch (message.action) {
+			case "command":
+				sendMessageToTarget(message);
+				console.log("main:".padEnd(10), message);
+				break;
+			case "heartbeat":
+				main.check = true;
+				main.heartbeat = 0;
+				break;
+			case "return":
+				console.log("main:".padEnd(10), message);
+		}
+	});
+
+	user.process.on('message', function (message) {
+		switch (message.action) {
+			case "command":
+				sendMessageToTarget(message);
+				console.log("user:".padEnd(10), message);
+				break;
+			case "heartbeat":
+				user.check = true;
+				user.heartbeat = 0;
+				break;
+			case "return":
+				console.log("user:".padEnd(10), message);
+		}
+	});
+
+	markov.process.on('exit', function (code) {
+		if (markov.invoked) return;
+		markov.invoked = true;
+		var err = code === 0 ? null : new Error('exit code ' + code);
+		callback(err);
+	});
+
+	admin.process.on('exit', function (code) {
+		if (admin.invoked) return;
+		admin.invoked = true;
+		var err = code === 0 ? null : new Error('exit code ' + code);
+		callback(err);
+	});
+
+	user.process.on('exit', function (code) {
+		if (user.invoked) return;
+		user.invoked = true;
+		var err = code === 0 ? null : new Error('exit code ' + code);
+		callback(err);
+	});
+
+	main.process.on('exit', function (code) {
+		if (main.invoked) return;
+		main.invoked = true;
+		var err = code === 0 ? null : new Error('exit code ' + code);
+		callback(err);
+	});
+
+
+	game.process.on('exit', function (code) {
+		if (game.invoked) return;
+		game.invoked = true;
+		var err = code === 0 ? null : new Error('exit code ' + code);
+		callback(err);
+	});
+}
+
+function processerror(proc) {
+	if(proc.invoked) return proc;
+	proc.invoked = true;
+	callback(err);
+	return proc
+
+}
+
+function sendMessageToTarget(message) {
+	
+	switch(message.target) {
+		case "markov":
+			markov.process.send(message);
+			break;
+		case "main":
+			main.process.send(message);
+			break;
+		case "user":
+			user.process.send(message);
+			break;
+		case "admin":
+			admin.process.send(message);
+			break;
+		case "game":
+			game.process.send(message);
+			break;
+		case "all":
+			markov.process.send(message);
+			main.process.send(message);
+			user.process.send(message);
+			admin.process.send(message);
+			game.process.send(message);
+			break;
+		case "watchdog": 
+			handleMessage(message);
+	}
+}
+
+function handleMessage(message) {
+	switch (message.command) {
+		case "restart":
+			restart(message);
+	}	 
+}
